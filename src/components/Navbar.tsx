9eda4a22381a4aca0a4a10368ff91462349c7c0b
SiddharthaArepalli <asiddharthagoud@gmail.com>
2025-01-08 23:17:13 +0530

    made changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
               href="#"
               className="text-2xl font-bold bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent"
             >
-              Portfolio
+              ASG
             </a>
 
             {/* Desktop Menu */}
